b7d6394f8da83b6454c22e136ea3c035511ca0e1
Oleksandr Kravets <a@kra.vc>
2025-04-11 13:12:52 +0300

    Add extra types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,20 @@
 declare type SchemaAttribute = object;
+export declare type SchemaAttributes = Record<string, SchemaAttribute>;
+declare type Enum = { enum: string[] };
+declare type Source = Enum | SchemaAttributes;
 
 export declare class Schema {
   constructor(
-    source: Record<string, SchemaAttribute>,
+    source: Source,
     id: string,
     url?: string
   )
+
+  static get id(): string;
+
+  only(propertyNames: string[], id?: string): Schema;
+  wrap(propertyName: string, options?: Record<string, any>, id?: string): Schema;
+  extend(properties: Record<string, any>, id?: string): Schema;
 }
 
 export declare class Validator {
